5a555dee8a4d4120ae2bb341d932e1b48d484a36
Pieter Provoost <pieterprovoost@gmail.com>
2021-06-09 12:31:17 +0200

    taxonomy fixes

--- occurrence.sql
+++ occurrence.sql
@@ -1,31 +1,47 @@
 select
-	concat('HAEDAT:', grid.gridCode, ':', eventName) as eventID,
-	concat('HAEDAT:', grid.gridCode, ':', eventName, ':causative:', ehcs.causativeSpeciesID) as occurrenceID,
-	comments as occurrenceRemarks,
-	species.name as scientificName,
-	cells_litre as organismQuantity,
-	case when nullif(cells_litre, '') is not null then 'cells per litre' else '' end as organismQuantityType,
+	eventID,
+	occurrenceID,
+	occurrenceRemarks,
+	scientificName,
+	case
+		when scientificName = 'Microcystis spp.' then 'urn:lsid:marinespecies.org:taxname:146557'
+		when scientificName = 'Microcystis sp.' then 'urn:lsid:marinespecies.org:taxname:146557'
+		when scientificName = 'Nitzschia sp.' then 'urn:lsid:marinespecies.org:taxname:149045'
+		when scientificName = 'Dolichospermum spp.' then 'urn:lsid:marinespecies.org:taxname:578476'
+		when scientificName = 'Dinophysis ovum Schütt sensu Martin 1929' then 'urn:lsid:marinespecies.org:taxname:109642'
+		when scientificName = 'Gonyaulax spinifera' then 'urn:lsid:marinespecies.org:taxname:110041'
+		else null
+	end as scientificNameID,
+	organismQuantity,
+	organismQuantityType,
 	'HumanObservation' as basisOfRecord,
 	'present' as occurrenceStatus
-from event
-inner join event_has_grid eg on eg.`event_id_events` = event.`id_events`
-left join grid on eg.`grid_gridCode` = grid.gridCode
-left join event_has_causative_species ehcs on ehcs.event_id_events = event.id_events 
-left join species on species.speciesID = ehcs.species_speciesID 
-where species.name is not null
-union all
-select
-	concat('HAEDAT:', grid.gridCode, ':', eventName) as eventID,
-	concat('HAEDAT:', grid.gridCode, ':', eventName, ':additional:', ehas.additionalSpeciesID) as occurrenceID,
-	comments as occurrenceRemarks,
-	species.name as scientificName,
-	cells_litre as organismQuantity,
-	case when nullif(cells_litre, '') is not null then 'cells per litre' else '' end as organismQuantityType,
-	'HumanObservation' as basisOfRecord,
-	'present' as occurrenceStatus
-from event
-inner join event_has_grid eg on eg.`event_id_events` = event.`id_events`
-left join grid on eg.`grid_gridCode` = grid.gridCode
-left join event_has_additional_species ehas on ehas.event_id_events = event.id_events 
-left join species on species.speciesID = ehas.species_speciesID 
-where species.name is not null;
+from
+(
+	select
+		concat('HAEDAT:', grid.gridCode, ':', eventName) as eventID,
+		concat('HAEDAT:', grid.gridCode, ':', eventName, ':causative:', ehcs.causativeSpeciesID) as occurrenceID,
+		comments as occurrenceRemarks,
+		species.name as scientificName,
+		cells_litre as organismQuantity,
+		case when nullif(cells_litre, '') is not null then 'cells per litre' else '' end as organismQuantityType
+	from event
+	inner join event_has_grid eg on eg.`event_id_events` = event.`id_events`
+	left join grid on eg.`grid_gridCode` = grid.gridCode
+	left join event_has_causative_species ehcs on ehcs.event_id_events = event.id_events 
+	left join species on species.speciesID = ehcs.species_speciesID 
+	union all
+	select
+		concat('HAEDAT:', grid.gridCode, ':', eventName) as eventID,
+		concat('HAEDAT:', grid.gridCode, ':', eventName, ':additional:', ehas.additionalSpeciesID) as occurrenceID,
+		comments as occurrenceRemarks,
+		species.name as scientificName,
+		cells_litre as organismQuantity,
+		case when nullif(cells_litre, '') is not null then 'cells per litre' else '' end as organismQuantityType
+	from event
+	inner join event_has_grid eg on eg.`event_id_events` = event.`id_events`
+	left join grid on eg.`grid_gridCode` = grid.gridCode
+	left join event_has_additional_species ehas on ehas.event_id_events = event.id_events 
+	left join species on species.speciesID = ehas.species_speciesID 
+) t
+where scientificName is not null;
